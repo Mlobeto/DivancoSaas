370e722313ee928234a94675ffd12d2c43a08d7e
mlobeto <mercedeslobeto@gmail.com>
2026-02-14 18:44:30 -0300

    xzzz

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
     alias: {
       "@": path.resolve(__dirname, "./src"),
       "@/core": path.resolve(__dirname, "./src/core"),
